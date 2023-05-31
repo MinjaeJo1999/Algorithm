0b98694a2ce460221162f1c9d0d0d1a5fb65b908
MinjaeJo1999 <76865900+MinjaeJo1999@users.noreply.github.com>
2023-05-31 23:52:49 +0900

    [5월 31일] 내 풀이 업데이트

--- 프로그래머스/Level1/옹알이 (2).py
+++ 프로그래머스/Level1/옹알이 (2).py
@@ -25,6 +25,47 @@ def solution(babbling):
 
 def my_solution(babbling):
     answer = 0
+    dic = ['aya','ye','woo','ma']
+    for target in babbling :
+        s = 0
+        e = 2 # 단어 최소 두자리이므로
+        before = ''
+        flag = True
+        while True :
+            if e > len(target) :
+                flag = False
+                break
+            if target[s:e] in dic :
+                if before == target[s:e] :
+                    flag = False
+                    break
+                else :
+                    before = target[s:e]
+                    flag = True
+                    if e == len(target) : break
+                s = e
+                e = s+2
+            else :
+                e += 1
+                flag = False
+        if flag :
+            answer += 1
+    return answer
+
+    # 유의 : 연속한 같은 발음 찾기 (불연속이면 상관없음)
+    # for문
+    # while문: 단어별로 발음할 수 있는지 check
+    # idx 0 부터 end index 늘려가면서 dic에 있는지 체크
+    # -> 있다면 다음 시작 인덱스는 마지막 인덱스 다음으로 갱신
+    # -> 이전 단어 뭐였는지 저장
+    # 탈출 조건 : end 값이 단어 길이보다 클 때
+    #   > 해당 조건이 유효하기 위해선 ayaye 와 ayaa인 케이스 구분해야 함
+    # flag 통해 answer 추가 여부 판단
+    # 알맞은 flag True / False 거쳐야 함
+    #   > 답이 되는 것 : end == len(단어)인 순간이 무조건 있음
+
+def my_solution1(babbling):
+    answer = 0
     dic = ['aya', 'ye','woo','ma']
     for word in babbling :
         a = 0
